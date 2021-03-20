f4ed3a5611eb24b474f67acde21a4bf2ce981a42
Cindy3184 <cindykrampah@yahoo.com>
2021-03-19 23:18:06 -0400

    added forms

--- server.js
+++ server.js
@@ -1,23 +1,43 @@
 const express = require('express');
 const { model } = require('mongoose');
+const path = require('path');
 const app = express();
 
+app.use(express.urlencoded({extended: false}));
+
+app.use(express.static('public'));
+app.use('/stylesheet', express.static(__dirname + 'public/stylesheet'));
+
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+
+
 app.get('/', (req, res) => {
-    res.send( 'Welcome to our homepage')
+    res.render('index')
 })
 
 
 app.get('/about', (req, res) => {
-    res.send('About Us')
+    res.render('about')
 })
 
 app.get('/menu', (req, res) => {
-    res.send('Menu')
+    res.render('menu')
 })
 
-app.get('order/', (req, res) => {
-    res.send('Place an Order!')
+app.get('/order', (req, res) => {
+    res.render('order')
 })
+
+app.post('/confirmed', (req, res) => {
+    // req.body.order
+    res.render('confirmed')
+})
+
 
 model.export = express
-app.listen(4000);
+app.listen(3000);
